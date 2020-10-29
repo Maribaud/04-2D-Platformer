35f7328bcd1021b784ef5805d12e3fd39899c015
Mark Ribaudo <maribaud@iu.edu>
2020-10-29 17:45:04 -0400

    A final look at a 2-D platformer

--- Levels/Exit.gd
+++ Levels/Exit.gd
@@ -1,15 +1,9 @@
 extends Area2D
 
 
-func _on_Exit_body_entered(body):
+func _on_Exit_to_3_body_entered(body):
 	if body.name == "Player":
 		if name == "Exit_to_2":
 			var _target = get_tree().change_scene("res://Levels/Level2.tscn")
 		if name == "Exit_to_3":
-			var _target = get_tree().change_scene("res://Levels/Level3.tscn")
-		if name == "Exit_to_4":
 			var _target = get_tree().change_scene("res://Levels/Game_Over.tscn")
-
-
-func _on_Exit_to_3_body_entered(body):
-	pass # Replace with function body.
